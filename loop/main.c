2a2d22bdc5db9ecb87f995cd8dbeb716d9591a4e
Victor S Macedo <guivi2000.vm@gmail.com>
2023-03-06 12:17:41 +0000

    Aula 06/03

--- loop/main.c
+++ loop/main.c
@@ -22,9 +22,9 @@ extern Int16 AIC3204_rset( Uint16 regnum, Uint16 regval);
 void main( void )
 
 {
-    Int16 r = 0,s, rmax, rmin,delta, delta0, deltamin, deltamax,gain = 32767,SDD,t,k = 8192,
-    look_sen[32]= {0,3212,6393 ,9512 ,12539,15446,18204,20787,23170,25329,27245,28898,30273,31356,32137,32609,
-    32767,32609,32137,31356,30273,28898,27245,25329,23170,20787,18204,15446,12539, 9512,6393,3212};
+    Int16 r = 0,s, rmax, rmin,delta, delta0, deltamin, deltamax,gain = 32767,SDD,t,k = 0,y1=0,y2,f,
+    look_sen[33]= {0,3212,6393 ,9512 ,12539,15446,18204,20787,23170,25329,27245,28898,30273,31356,32137,32609,
+    32767,32609,32137,31356,30273,28898,27245,25329,23170,20787,18204,15446,12539, 9512,6393,3212,0};
     // Definiçao do Delta = 4,096 * Fo
     delta0 = 16384;                             //Variavel Delta a ser incrementada
     deltamin = 8192;                            //4.096*2000
@@ -120,13 +120,18 @@ s =s >> 10;
 
 
 t = look_sen[s];
+y2 = look_sen[s+1];
 if(r<0){
     t = -t;
+    y2= -y2;
 }
+y1 = t;
+f = (r & 1023) << 5;           //0b0000 0011 1111 1111;
+DataOutLeft = y1+((((y2-y1)*(long)f)<<1)>>16);
 
 SDD =(((long)gain * t)<<1) >> 16;
-DataOutLeft = SDD;       // loop left channel samples
-DataOutRight = r;         // loop right channel samples
+//DataOutLeft = SDD;       // loop left channel samples
+DataOutRight = y1;         // loop right channel samples
 //--------------------------------------------------------------------------------------------------------------------
 // Your program here!!!
 //--------------------------------------------------------------------------------------------------------------------
